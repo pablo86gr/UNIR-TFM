19e135813d7d19a9653a94e25cf7af691e86b720
pablo86gr <86490260+pablo86gr@users.noreply.github.com>
2021-06-25 19:48:07 +0200

    Add files via upload

---
+++ trends.py
@@ -0,0 +1,32 @@
+#Obtenido de https://www.honchosearch.com/blog/seo/how-to-use-python-pytrends-to-automate-google-trends-data/
+
+from pytrends.request import TrendReq
+import pandas as pd
+import time
+startTime = time.time()
+pytrend = TrendReq(hl='en-GB', tz=360)
+
+colnames = ["keywords"]
+df = pd.read_csv("trends\keyword_list.csv", names=colnames)
+df2 = df["keywords"].values.tolist()
+# df2.remove("keywords")
+
+dataset = []
+
+for x in range(0,len(df2)):
+     keywords = [df2[x]]
+     pytrend.build_payload(
+          kw_list=keywords,
+          cat=0,
+          timeframe='2009-01-01 2021-05-01',
+          geo='SN')
+     data = pytrend.interest_over_time()
+     if not data.empty:
+          data = data.drop(labels=['isPartial'],axis='columns')
+          dataset.append(data)
+
+result = pd.concat(dataset, axis=1)
+result.to_csv('search_trends_SN.csv')
+
+executionTime = (time.time() - startTime)
+print('Execution time in sec.: ' + str(executionTime))
